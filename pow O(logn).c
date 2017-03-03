eff77b64f0de70e2b39286b5ca5c795c849b3c4c
hhalilaaydin <hhalilaaydin@gmail.com>
2017-03-03 13:06:35 +0200

    Update pow O(logn).c

--- pow O(logn).c
+++ pow O(logn).c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
-{
-    int z= pow(4,10);
-    printf("%d",z);
-    return 0;
-}
+int sayac;
 int pow(int x,int n){
+    sayac++;
     if (n==0)           //+1
         return 1;
     else if (x==1)      // --
@@ -26,4 +21,11 @@ int pow(int x,int n){
 int even(int y){        // --
     if ((y%2)==0) return 1;
     else return 0;
+}
+int main()
+{
+    printf("Sayac=  %d",sayac);
+    int z= pow(4,10);
+    printf("%d",z);
+    return 0;
 }
